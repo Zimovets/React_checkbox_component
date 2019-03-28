e4c09b893d5af991a092f40a35225fae2fa0e73a
Zimovets Sergey <zimovets_s@ukr.net>
2019-03-28 22:26:40 +0200

    - create CheckBox component

---
+++ src/components/CheckBox/index.jsx
@@ -0,0 +1,45 @@
+import React, { Component, PropTypes } from 'react';
+import './checkBox.css';
+
+class Checkbox extends Component {
+  state = {
+    isChecked: false,
+  }
+
+  toggleCheckboxChange = () => {
+    const { handleCheckboxChange, label } = this.props;
+
+    this.setState(({ isChecked }) => (
+      {
+        isChecked: !isChecked,
+      }
+    ));
+
+    handleCheckboxChange(label);
+  }
+
+  render() {
+    const { label } = this.props;
+    const { isChecked } = this.state;
+
+    return (
+      <div className="checkbox">
+        <label class="container">{label}
+          <input type="checkbox" 
+            checked="checked" 
+            value={label}
+            checked={isChecked}
+            onChange={this.toggleCheckboxChange}/>
+          <span class="checkmark"></span>
+        </label>
+      </div>
+    );
+  }
+}
+
+Checkbox.propTypes = {
+  label: PropTypes.string.isRequired,
+  handleCheckboxChange: PropTypes.func.isRequired,
+};
+
+export default Checkbox;
