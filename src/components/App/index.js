19b6a95b19e5d06cbabfdc3c590605b5d72be5bb
Zimovets Sergey <zimovets_s@ukr.net>
2019-03-29 14:01:26 +0200

    - fix App component, fix props

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CheckBox label="CheckBox1" isChecked handleCheckboxChange={() => console.log('test')}/>
-        <CheckBox label="CheckBox2"  handleCheckboxChange={() => console.log('test')}/>
+        <CheckBox label="CheckBox1" isChecked={true} handleCheckboxChange={() => console.log('test')}/>
+        <CheckBox label="CheckBox2"  isChecked={false} handleCheckboxChange={() => console.log('test')}/>
       </div>
     );
   }
