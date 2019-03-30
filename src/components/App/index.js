1c33cd9c2399de100a25a585cfb872322f7f6bfe
Zimovets Sergey <zimovets_s@ukr.net>
2019-03-30 14:06:23 +0200

    - change App component, change checked property to defaultChecked

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CheckBox value="CheckBox1" checked onChange={() => console.log('test')}/>
-        <CheckBox value="CheckBox2"  onChange={() => console.log('test')}/>
+        <CheckBox value="CheckBox1" defaultChecked={true} onChange={() => console.log('test')}/>
+        <CheckBox value="CheckBox2"  defaultChecked={false} onChange={() => console.log('test')}/>
       </div>
     );
   }
