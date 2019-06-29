d46c57919ef44adb786a802b83073b2a40462f49
пк <zimovets_s@ukr.net>
2019-06-29 12:10:12 +0300

    Added styled-component librery - changed App component, delete extra code

---
+++ src/containers/App/index.js
@@ -0,0 +1,17 @@
+//core
+import React, { Component } from 'react';
+
+// Styles
+import { Main } from './styles';
+
+class App extends Component {
+  render() {
+    return (
+      <Main>
+        <div>Hello</div>
+      </Main>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/App/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Main = styled.div`
+    width: 800px;
+    height: 400px;
+    margin-top: 100px;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+`;
