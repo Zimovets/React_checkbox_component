0504f8526994877911036b5ed2a8ddb3a1d472cf
пк <zimovets_s@ukr.net>
2019-06-30 15:46:07 +0300

    Updates

---
+++ precache-manifest.3dd5b4f2ab391207b29018a54c709bff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0113f455bfd95f2d29f",
+    "url": "/React_checkbox_component/static/js/runtime~main.0b442754.js"
+  },
+  {
+    "revision": "ebb2663416b8c35a9599",
+    "url": "/React_checkbox_component/static/js/main.acfdd557.chunk.js"
+  },
+  {
+    "revision": "6efee66a8e9b2d26a11f",
+    "url": "/React_checkbox_component/static/js/2.b898d4b2.chunk.js"
+  },
+  {
+    "revision": "ebb2663416b8c35a9599",
+    "url": "/React_checkbox_component/static/css/main.8f9f92a9.chunk.css"
+  },
+  {
+    "revision": "98a35dfa86ecbdc0ef50b46a72e921a3",
+    "url": "/React_checkbox_component/index.html"
+  }
+];
