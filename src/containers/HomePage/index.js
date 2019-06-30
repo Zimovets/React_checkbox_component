c86de6053b8420e76293e7cda9f3deaf018ed3d1
пк <zimovets_s@ukr.net>
2019-06-30 12:40:50 +0300

    Remove InputSection component conttent to HomePage component

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,14 +1,44 @@
 //core
 import React, { Component } from 'react'
 
-//components
-import InputSection from './InputSection';
+//component
+import GifCard from './GifCard'; 
+
+//services
+import { getGifsbyKeyWord } from '../../services/GiphyService';
+
+// Styles
+import { InputArea, InputField, InputButton, ContentArea } from './styles';
 
 export default class HomePage extends Component {
+    state = {
+        inputText: '',
+        result: [],
+    }
+
+    onClick = () => {
+        const { inputText } = this.state;
+        getGifsbyKeyWord(inputText).then(res => this.setState({result: res.data.map(el => el.images.original.url)}));
+    }
+
+    onEnterPress = e => {
+        const { inputText } = this.state;
+        if (e.key === 'Enter') {
+            getGifsbyKeyWord(inputText).then(res => this.setState({result: res.data.map(el => el.images.original.url)}));    
+        }
+    }
+
     render() {
+        const { result } = this.state
         return (
             <React.Fragment>
-                <InputSection/>
+                <InputArea>
+                    <InputField onKeyDown={this.onEnterPress} onChange={e => this.setState({inputText: e.target.value})}/>
+                    <InputButton onClick={this.onClick}>Find</InputButton>
+                </InputArea>
+                <ContentArea>
+                    {result.map(item => <GifCard key={item} path={item}/>)}
+                </ContentArea>
             </React.Fragment>
         )
     }
